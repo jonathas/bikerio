b8780b4aff2e22a4e82e8ad0ebdb66c40d414929
Rodrigo Silva <rodrigo.silva@solucoesglobais.com.br>
2014-07-17 17:35:11 -0300

    exibindo no menu as estações disponiveis

--- js/application.js
+++ js/application.js
@@ -1,35 +1,26 @@
 var App = {
+    stations: {},
     map: {},
-    sort: function($element) {
-        $element.html($element.find('option').sort(function(a, b) {
-            return a.text === b.text ? 0 : a.text < b.text ? -1 : 1;
-        }));
-    },
-    template: function($id, $Name, $Description, $Start, $Finish) {
-        return '<div class="item item-avatar">\
-                    <img src="img/no_image.png"/>\
-                    <h2>' + $Name + '</h2>\
-                    <p>' + $Start + ' a ' + $Finish + '</p>\
-                </div>\
-                <div class="item item-body">\
-                    <p>' + $Description + ' </p>\
-                </div>\
-                <div class="item tabs tabs-secondary tabs-icon-left" data-id_event="' + $id + '">\
-                    <a class="tab-item" href="#" data-function="open">\
-                        <i class="icon ion-location"></i>\
-                        Ver\
-                    </a>\
-<!--                <a class="tab-item" href="#" data-function="save">\
-                        <i class="icon ion-calendar"></i>\
-                        Salvar\
-                    </a>\
-                    <a class="tab-item" href="#" data-function="share">\
-                        <i class="icon ion-share"></i>\
-                        Compartilhar\
-                    </a>-->\
+    template: function($id, $Name) {
+        var $temp_name = $Name.replaceAll('_', ' ');
+        return '<div data-id_statios="' + $id + '" class="item item-button-right">\
+                    ' + $temp_name + '\
+                    <button class="button button-positive">\
+                        <i class="icon ion-arrow-right-c"></i>\
+                    </button>\
                 </div>';
     },
+    order_by_name: function() {
+        App.stations.sort(function(a, b) {
+            if (a.nome > b.nome)
+                return 1;
+            if (a.nome < b.nome)
+                return -1;
+            return 0;
+        });
+    },
     apply_events: function() {
+        $("main#main menu article").empty();
         var markers = new OpenLayers.Layer.Markers("Markers");
         $.ajax({
             url: 'http://datapoa.com.br/api/action/datastore_search',
@@ -38,13 +29,27 @@ var App = {
             },
             dataType: 'jsonp',
             success: function($response) {
-                var $records = $response.result.records;
-                $.each($records, function($key, $event) {
-                    var $geolocation = new OpenLayers.LonLat($event.LONGITUDE, $event.LATITUDE).transform(new OpenLayers.Projection("EPSG:4326"), App.map.getProjectionObject());
+                App.stations = $response.result.records;
+                var $items = [];
+                App.order_by_name();
+                $.each(App.stations, function($key, $station) {
+                    var $geolocation = new OpenLayers.LonLat($station.LONGITUDE, $station.LATITUDE).transform(new OpenLayers.Projection("EPSG:4326"), App.map.getProjectionObject());
                     var $Marker = new OpenLayers.Marker($geolocation);
                     markers.addMarker($Marker);
+                    $items.push(App.template($station.numero, $station.nome));
                 });
                 App.map.addLayer(markers);
+                App.stations.sort(function(a, b) {
+                    return a['nome'] - b['nome'];
+                });
+                $("<div/>", {
+                    class: 'list card',
+                    html: $items.join("")
+                }).appendTo("main#main menu article").find('div.tabs a.tab-item').click(function() {
+                    var $function = $(this).data('function');
+                    var $id_event = $(this).closest('.item').data('id_event');
+                    App.log("App.View." + $function + "('" + $id_event + "')", true);
+                });
             }
         });
     },
@@ -53,7 +58,7 @@ var App = {
         App.apply_events();
         App.map = new OpenLayers.Map("map");
         App.map.addLayer(new OpenLayers.Layer.OSM());
-        var lonLat = new OpenLayers.LonLat( -51.22067189, -30.06074719).transform(new OpenLayers.Projection("EPSG:4326"), App.map.getProjectionObject());
+        var lonLat = new OpenLayers.LonLat(-51.22067189, -30.06074719).transform(new OpenLayers.Projection("EPSG:4326"), App.map.getProjectionObject());
         App.map.setCenter(lonLat, 12.5);
     }
 };
@@ -70,3 +75,12 @@ $(document).ready(function() {
 //        $('main#main section header.bar-header button').click();
 //    });
 });
+String.prototype.replaceAll = function(de, para) {
+    var str = this;
+    var pos = str.indexOf(de);
+    while (pos > -1) {
+        str = str.replace(de, para);
+        pos = str.indexOf(de);
+    }
+    return (str);
+}
